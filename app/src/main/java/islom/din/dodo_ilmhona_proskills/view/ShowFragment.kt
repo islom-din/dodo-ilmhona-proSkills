c5173ef07013feaca509c538e10d020bdca81e16
umedqws <120251717+umedqws@users.noreply.github.com>
2023-01-31 13:40:25 +0300

    version 2

--- app/src/main/java/islom/din/dodo_ilmhona_proskills/view/ShowFragment.kt
+++ app/src/main/java/islom/din/dodo_ilmhona_proskills/view/ShowFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,11 +11,13 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import islom.din.dodo_ilmhona_proskills.R
 import islom.din.dodo_ilmhona_proskills.databinding.ViewShowFragmentBinding
 import islom.din.dodo_ilmhona_proskills.model.Sous
+import islom.din.dodo_ilmhona_proskills.viewModel.MyViewModel
 
 class ShowFragment : Fragment() {
     private var fragmentFirstBinding: ViewShowFragmentBinding? = null
     lateinit var recyclerView: RecyclerView
     lateinit var listSousAdapter: ListSousAdapter
+    private val myViewModel= MyViewModel()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -31,24 +32,15 @@ class ShowFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val binding = view.let { ViewShowFragmentBinding.bind(it) }
 
+
+
         fragmentFirstBinding = binding
         recyclerView = binding.recSous
         listSousAdapter = ListSousAdapter()
         recyclerView.adapter = listSousAdapter
         recyclerView.layoutManager = StaggeredGridLayoutManager(3, LinearLayoutManager.VERTICAL)
-        val list = listOf<Sous>(
-            Sous(1, R.drawable.chedderpng, "sousoso", 25),
-            Sous(1, R.drawable.chedderpng, "sousoso", 25),
-            Sous(1, R.drawable.chedderpng, "sousoso", 25),
-            Sous(1, R.drawable.chedderpng, "sousoso", 25),
-            Sous(1, R.drawable.chedderpng, "sousoso", 25),
-            Sous(1, R.drawable.chedderpng, "sousoso", 25),
-            Sous(1, R.drawable.chedderpng, "sousoso", 25),
-            Sous(1, R.drawable.chedderpng, "sousoso", 25),
-            Sous(1, R.drawable.chedderpng, "sousoso", 25),
-            Sous(1, R.drawable.chedderpng, "sousoso", 25)
-        )
-        listSousAdapter.submitList(list)
+
+
 
         listSousAdapter.onClick={ it ->
             val newList = mutableListOf<Sous>()
@@ -71,20 +63,25 @@ class ShowFragment : Fragment() {
             binding.big.setBackgroundResource(R.drawable.back_selcted)
             binding.normal.setBackgroundResource(R.drawable.back_selcted)
 
+
+            listSousAdapter.submitList(myViewModel.getListCous())
+
+
         }
         binding.big.setOnClickListener {
             binding.small.setBackgroundResource(R.drawable.back_selcted)
             binding.big.setBackgroundResource(R.drawable.background_select)
             binding.normal.setBackgroundResource(R.drawable.back_selcted)
+            listSousAdapter.submitList(myViewModel.getListCous1())
         }
 
         binding.normal.setOnClickListener {
             binding.small.setBackgroundResource(R.drawable.back_selcted)
             binding.big.setBackgroundResource(R.drawable.back_selcted)
             binding.normal.setBackgroundResource(R.drawable.background_select)
+            listSousAdapter.submitList(myViewModel.getListCous2())
         }
 
-            binding.classic.isVisible = true
 
     }
 
