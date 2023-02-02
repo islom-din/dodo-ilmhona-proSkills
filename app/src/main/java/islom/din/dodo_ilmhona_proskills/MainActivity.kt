65671c5a786667777f3ca5b3064f062c76c5b776
Shodmon <shodik.9393@gmail.com>
2023-02-02 17:40:51 +0500

    init

--- app/src/main/java/islom/din/dodo_ilmhona_proskills/MainActivity.kt
+++ app/src/main/java/islom/din/dodo_ilmhona_proskills/MainActivity.kt
@@ -2,10 +2,12 @@ package islom.din.dodo_ilmhona_proskills
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
